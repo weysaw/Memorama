bd9e82e8ee29cb36687d1e4bbf5ed00599a6b4e6
Axel L Ornelas M <axel.lom17@gmail.com>
2021-10-07 14:33:53 -0700

    Initial commit

---
+++ app/src/main/java/uabc/axel/ornelas/memorama/Score.kt
@@ -0,0 +1,11 @@
+package uabc.axel.ornelas.memorama
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Score : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_score)
+    }
+}
